8d9661cc4012f52d016c98cbeefef79773bf9988
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2023-04-18 11:26:43 +0300

    Изменен способ инициализации свойств табов

--- app/src/main/java/com/geekbrains/universe/ui/pages/solarsystem/PagerActivity.kt
+++ app/src/main/java/com/geekbrains/universe/ui/pages/solarsystem/PagerActivity.kt
@@ -2,11 +2,26 @@ package com.geekbrains.universe.ui.pages.solarsystem
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.geekbrains.universe.R
 import com.geekbrains.universe.databinding.ActivityPagerBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class PagerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPagerBinding
+
+    private val tabTitleByPosition = mapOf<Int, String>(
+        0 to getString(R.string.tab_earth_title),
+        1 to getString(R.string.tab_mars_title),
+        2 to getString(R.string.tab_weather_title)
+    )
+
+    private val tabIconIdByPosition = mapOf<Int, Int>(
+        0 to R.drawable.ic_earth,
+        1 to R.drawable.ic_mars,
+        2 to R.drawable.ic_system
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +30,15 @@ class PagerActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.pager.adapter = PagerAdapter(this)
+        setTabs()
+    }
+
+    private fun setTabs() {
+        TabLayoutMediator(binding.tabs, binding.pager) { tab, position ->
+            tab.text = tabTitleByPosition[position]!!
+            tab.icon = ContextCompat.getDrawable(
+                this@PagerActivity, tabIconIdByPosition[position]!!
+            )
+        }
     }
 }
--- app/src/main/java/com/geekbrains/universe/ui/pages/solarsystem/PagerAdapter.kt
+++ app/src/main/java/com/geekbrains/universe/ui/pages/solarsystem/PagerAdapter.kt
@@ -7,23 +7,21 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 class PagerAdapter(fragmentActivity: FragmentActivity) :
     FragmentStateAdapter(fragmentActivity) {
 
-    private val fragments = arrayOf(EarthFragment(), MarsFragment(), WeatherFragment())
-
-    companion object {
-        private const val EARTH_FRAGMENT = 0
-        private const val MARS_FRAGMENT = 1
-        private const val WEATHER_FRAGMENT = 2
-    }
+    private val fragmentsByPosition = mapOf<Int, Fragment>(
+        0 to EarthFragment(),
+        1 to MarsFragment(),
+        2 to WeatherFragment(),
+    )
 
     override fun getItemCount(): Int {
-        return fragments.size
+        return fragmentsByPosition.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        return when (position) {
-            0 -> fragments[EARTH_FRAGMENT]
-            1 -> fragments[MARS_FRAGMENT]
-            else -> fragments[WEATHER_FRAGMENT]
-        }
+        return fragmentsByPosition[position]!!
     }
+
+//    override fun getPageTitle(position: Int): CharSequence? {
+//
+//    }
 }
