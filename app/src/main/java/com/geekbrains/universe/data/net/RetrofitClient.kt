2b3d301d0078caba9634d75ed71547c1c3da5343
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2023-04-21 21:49:13 +0300

    Реализована загрузка фото земли

--- app/src/main/java/com/geekbrains/universe/data/net/RetrofitClient.kt
+++ app/src/main/java/com/geekbrains/universe/data/net/RetrofitClient.kt
@@ -1,8 +1,8 @@
 package com.geekbrains.universe.data.net
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
-import okhttp3.Interceptor
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
--- app/src/main/java/com/geekbrains/universe/ui/pages/solarsystem/earth/EarthFragment.kt
+++ app/src/main/java/com/geekbrains/universe/ui/pages/solarsystem/earth/EarthFragment.kt
@@ -8,10 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import coil.load
 import com.geekbrains.universe.databinding.FragmentEarthBinding
-import com.geekbrains.universe.domain.EarthImagingCamera
 import com.geekbrains.universe.ui.AppState
 import com.geekbrains.universe.ui.utils.QueryDay
-import java.time.LocalDate
 
 class EarthFragment : Fragment() {
 
