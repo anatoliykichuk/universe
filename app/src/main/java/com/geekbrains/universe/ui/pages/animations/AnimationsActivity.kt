47aa0a0721d3564a954c5e63c4214d796f626191
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2023-04-28 18:18:51 +0300

    Реализован обработчик нажатия на кнопку в новом активити, для демонстрации работы Transitions

--- app/src/main/java/com/geekbrains/universe/ui/pages/animations/AnimationsActivity.kt
+++ app/src/main/java/com/geekbrains/universe/ui/pages/animations/AnimationsActivity.kt
@@ -1,12 +1,28 @@
 package com.geekbrains.universe.ui.pages.animations
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.TransitionManager
 import com.geekbrains.universe.R
+import com.geekbrains.universe.databinding.ActivityAnimationsBinding
 
 class AnimationsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAnimationsBinding
+    private var textIsVisible = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityAnimationsBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_animations)
+
+        binding.button.setOnClickListener {
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer)
+
+            textIsVisible != textIsVisible
+            binding.text.visibility = if (textIsVisible) View.VISIBLE else View.GONE
+        }
     }
 }
