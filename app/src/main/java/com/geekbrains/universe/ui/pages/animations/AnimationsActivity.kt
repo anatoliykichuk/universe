df6b4ea155c4b24014c597235551cf36c6fca57d
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2023-04-28 23:04:40 +0300

    Использован Transition типа Explode

--- app/src/main/java/com/geekbrains/universe/ui/pages/animations/AnimationsActivity.kt
+++ app/src/main/java/com/geekbrains/universe/ui/pages/animations/AnimationsActivity.kt
@@ -1,28 +1,72 @@
 package com.geekbrains.universe.ui.pages.animations
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.transition.Explode
+import android.transition.Transition
+import android.transition.TransitionManager
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.transition.TransitionManager
+import androidx.recyclerview.widget.RecyclerView
 import com.geekbrains.universe.R
-import com.geekbrains.universe.databinding.ActivityAnimationsBinding
+import com.geekbrains.universe.databinding.ActivityAnimationsExplodeBinding
+
+private const val ITEM_COUNT = 32
 
 class AnimationsActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityAnimationsBinding
-    private var textIsVisible = false
+    private lateinit var binding: ActivityAnimationsExplodeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityAnimationsBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_animations)
+        binding = ActivityAnimationsExplodeBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_animations_explode)
 
-        binding.button.setOnClickListener {
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer)
+        binding.recyclerView.adapter = Adapter()
+    }
 
-            textIsVisible != textIsVisible
-            binding.text.visibility = if (textIsVisible) View.VISIBLE else View.GONE
+    private fun explode(clickedView: View) {
+        val viewRect = Rect()
+        clickedView.getGlobalVisibleRect(viewRect)
+
+        val explode = Explode()
+        explode.epicenterCallback = object : Transition.EpicenterCallback() {
+            override fun onGetEpicenter(transition: Transition?): Rect {
+                return viewRect
+            }
+        }
+
+        explode.duration = 1000
+        TransitionManager.beginDelayedTransition(binding.recyclerView, explode)
+
+        binding.recyclerView.adapter = null
+    }
+
+    private inner class Adapter : RecyclerView.Adapter<ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.activity_animations_explode_recycler_view_item,
+                    parent,
+                    false
+                ) as View
+            )
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.itemView.setOnClickListener() {
+                explode(it)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return ITEM_COUNT
         }
     }
+
+    private inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
