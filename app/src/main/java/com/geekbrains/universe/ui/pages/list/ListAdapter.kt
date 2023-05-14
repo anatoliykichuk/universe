e06ffb3ce2b0985e82bf910abcc842bef4040cfc
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2023-05-14 11:10:22 +0300

    Усилен эффект перетаскивания посредством констраста цветов фона

--- app/src/main/java/com/geekbrains/universe/ui/pages/list/ListAdapter.kt
+++ app/src/main/java/com/geekbrains/universe/ui/pages/list/ListAdapter.kt
@@ -10,7 +10,6 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.geekbrains.universe.R
 import com.geekbrains.universe.domain.ItemData
-import java.security.PrivateKey
 
 class ListAdapter(
     private val onListItemClickListener: OnListItemClickListener,
