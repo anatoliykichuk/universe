71f362be71474177dc0b161b3f5592ed2ec8f4e0
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2023-05-19 14:36:38 +0300

    Добавлена анимация начального экрана

--- app/src/main/java/com/geekbrains/universe/ui/pages/splash/SplashActivity.kt
+++ app/src/main/java/com/geekbrains/universe/ui/pages/splash/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import com.geekbrains.universe.databinding.ActivitySplashBinding
 import com.geekbrains.universe.ui.MainActivity
@@ -13,12 +14,17 @@ class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
     private val handler = Handler(Looper.getMainLooper())
     private val delay = 3000L
+    private val rotateValue = 750f
+    private val interpolatorDuration = 10000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.splashImage.animate().rotationBy(rotateValue)
+            .setInterpolator(LinearInterpolator()).duration = interpolatorDuration
 
         handler.postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
